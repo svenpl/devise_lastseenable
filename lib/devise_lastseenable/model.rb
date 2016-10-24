abbcc4234527a3e5e02c92960c92582743952cbb
Sven Plock <sven.plock@liemak.de>
2016-10-24 17:07:28 +0200

    Remove timelimit for lastseen update

--- lib/devise_lastseenable/model.rb
+++ lib/devise_lastseenable/model.rb
@@ -4,10 +4,10 @@ module Devise
   module Models
     module Lastseenable
       def stamp!
-        if self.last_seen.to_i < (Time.now - 5.minutes).to_i
-          self.last_seen = DateTime.now
-          self.save!
-        end
+        # if self.last_seen.to_i < (Time.now - 5.minutes).to_i
+        self.last_seen = DateTime.now
+        self.save!
+        # end
       end
     end
   end
