67e1e55186b9819ac27ada348890ccebf09a5926
Sven Plock <sven.plock@liemak.de>
2016-10-25 17:44:39 +0200

    Rescue from StaleErrorObject The error can be caused when multiple threads try update the users `last_seen` attribute. This does not cause any issues because another thread has already updated it then.

--- lib/devise_lastseenable/model.rb
+++ lib/devise_lastseenable/model.rb
@@ -4,15 +4,14 @@ module Devise
   module Models
     module Lastseenable
       def stamp!
-        if self.last_seen.to_i < (Time.now - 5.seconds).to_i
+        if self.last_seen.to_i < (Time.now - 5.minutes).to_i
           begin
             self.last_seen = DateTime.now
             self.save!
-          rescue ActiveRecord::StaleObjectError => e
-            Rails.logger.info "Something went wrong but we still just continue"
-            Rails.logger.info "**"*20
-            Rails.logger.info e
-            Rails.logger.info "**"*20
+          rescue ActiveRecord::StaleObjectError => error
+            # Because of multi threading this can be called multiple times in different threads and can cause a `StaleObjectError`.
+            # This error can be ignored because it only prevents the users `last_seen` attribute to update and does not harm the application.
+            # The `last_seen` attribute was already updated in another thread anyways.
           end
         end
       end
